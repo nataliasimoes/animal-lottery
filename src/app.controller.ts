73f415c4d44b7f33629ae3d8f9873d691d40edb9
nataliasimoes <natlia932@gmail.com>
2023-02-09 08:34:48 -0300

    Add some comments

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,38 +1,42 @@
-import { Controller, Inject, Sse } from '@nestjs/common';
-import { AppService } from './app.service';
-import { Observable } from 'rxjs';
-import { RedisClientType } from '@redis/client';
+import { Controller, Inject, Sse } from "@nestjs/common";
+import { AppService } from "./app.service";
+import { Observable } from "rxjs";
+import { RedisClientType } from "@redis/client";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService, @Inject('CACHE_MANAGER') private redisClient : RedisClientType) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject("CACHE_MANAGER") private redisClient: RedisClientType
+  ) {}
 
   @Sse("watch")
-    watchVariable() : Observable<MessageEvent> {
-      //criar um enventsource no browser para ficar escutando
-      return new Observable(subscriber => {
-        (async () => {
-          let client;
-          try {
-            client = await this.redisClient.duplicate();
-            client.connect().then(() => {
-              this.redisClient.subscribe("result", (message, channel) => {
-                const messageEvent = new MessageEvent("message", {data: message});
-                subscriber.next(messageEvent)
-              })
-            })
-          } catch (error) {
-            console.log(error);
-            
-          }
-          return () => {
-            console.log("Disconectou...");
-            
-            subscriber.unsubscribe();
-            client.disconnect();
-          }
-        })();
-      })
-  }
+  watchVariable(): Observable<MessageEvent> {
+    //criar um enventsource no browser para ficar escutando
+    return new Observable((subscriber) => {
+      (async () => {
+        let client;
+        try {
+          // O método duplicate cria um novo cliente e o método connect é chamado para conectar esse cliente ao Redis.
+          client = await this.redisClient.duplicate();
+          client.connect().then(() => {
+            this.redisClient.subscribe("result", (message, channel) => {
+              const messageEvent = new MessageEvent("message", {
+                data: message,
+              });
+              subscriber.next(messageEvent);
+            });
+          });
+        } catch (error) {
+          console.log(error);
+        }
+        return () => {
+          console.log("Disconnected...");
 
+          subscriber.unsubscribe();
+          client.disconnect();
+        };
+      })();
+    });
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,45 +1,50 @@
-import { Injectable } from '@nestjs/common';
-import { ClientOptions, ClientProxy, ClientProxyFactory, Transport } from '@nestjs/microservices';
-import { interval, map } from 'rxjs';
+import { Injectable } from "@nestjs/common";
+import {
+  ClientOptions,
+  ClientProxy,
+  ClientProxyFactory,
+  Transport,
+} from "@nestjs/microservices";
+import { interval, map } from "rxjs";
 
 // define as opções de conexão com o microserviço
-const clientOptions : ClientOptions = {
+const clientOptions: ClientOptions = {
   transport: Transport.REDIS,
   options: {
-    host: 'localhost',
+    host: "localhost",
     port: 6379,
-    retryAttempts : 5,
-    retryDelay : 5000
+    retryAttempts: 5,
+    retryDelay: 5000,
   },
-}
+};
 
 const lottery: Animal[] = [
-  { name: 'Avestruz', group: 1, dozens: ['01', '02', '03', '04'] },
-  { name: 'Águia', group: 2, dozens: ['05', '06', '07', '08'] },
-  { name: 'Burro', group: 3, dozens: ['09', '10', '11', '12'] },
-  { name: 'Borboleta', group: 4, dozens: ['13', '14', '15', '16'] },
-  { name: 'Cachorro', group: 5, dozens: ['17', '18', '19', '20'] },
-  { name: 'Cabra', group: 6, dozens: ['21', '22', '23', '24'] },
-  { name: 'Carneiro', group: 7, dozens: ['25', '26', '27', '28'] },
-  { name: 'Camelo', group: 8, dozens: ['29', '30', '31', '32'] },
-  { name: 'Cobra', group: 9, dozens: ['33', '34', '35', '36'] },
-  { name: 'Coelho', group: 10, dozens: ['37', '38', '39', '40'] },
-  { name: 'Cavalo', group: 11, dozens: ['41', '42', '43', '44'] },
-  { name: 'Elefante', group: 12, dozens: ['45', '46', '47', '48'] },
-  { name: 'Galo', group: 13, dozens: ['49', '50', '51', '52'] },
-  { name: 'Gato', group: 14, dozens: ['53', '54', '55', '56'] },
-  { name: 'Jacaré', group: 15, dozens: ['57', '58', '59', '60'] },
-  { name: 'Leão', group: 16, dozens: ['61', '62', '63', '64'] },
-  { name: 'Macaco', group: 17, dozens: ['65', '66', '67', '68'] },
-  { name: 'Porco', group: 18, dozens: ['69', '70', '71', '72'] },
-  { name: 'Pavão', group: 19, dozens: ['73', '74', '75', '76'] },
-  { name: 'Peru', group: 20, dozens: ['77', '78', '79', '80'] },
-  { name: 'Touro', group: 21, dozens: ['81', '82', '83', '84'] },
-  { name: 'Tigre', group: 22, dozens: ['85', '86', '87', '88'] },
-  { name: 'Urso', group: 23, dozens: ['89', '90', '91', '92'] },
-  { name: 'Veado', group: 24, dozens: ['93', '94', '95', '96'] },
-  { name: 'Vaca', group: 25, dozens: ['97', '98', '99', '00'] },
-] 
+  { name: "Avestruz", group: 1, dozens: ["01", "02", "03", "04"] },
+  { name: "Águia", group: 2, dozens: ["05", "06", "07", "08"] },
+  { name: "Burro", group: 3, dozens: ["09", "10", "11", "12"] },
+  { name: "Borboleta", group: 4, dozens: ["13", "14", "15", "16"] },
+  { name: "Cachorro", group: 5, dozens: ["17", "18", "19", "20"] },
+  { name: "Cabra", group: 6, dozens: ["21", "22", "23", "24"] },
+  { name: "Carneiro", group: 7, dozens: ["25", "26", "27", "28"] },
+  { name: "Camelo", group: 8, dozens: ["29", "30", "31", "32"] },
+  { name: "Cobra", group: 9, dozens: ["33", "34", "35", "36"] },
+  { name: "Coelho", group: 10, dozens: ["37", "38", "39", "40"] },
+  { name: "Cavalo", group: 11, dozens: ["41", "42", "43", "44"] },
+  { name: "Elefante", group: 12, dozens: ["45", "46", "47", "48"] },
+  { name: "Galo", group: 13, dozens: ["49", "50", "51", "52"] },
+  { name: "Gato", group: 14, dozens: ["53", "54", "55", "56"] },
+  { name: "Jacaré", group: 15, dozens: ["57", "58", "59", "60"] },
+  { name: "Leão", group: 16, dozens: ["61", "62", "63", "64"] },
+  { name: "Macaco", group: 17, dozens: ["65", "66", "67", "68"] },
+  { name: "Porco", group: 18, dozens: ["69", "70", "71", "72"] },
+  { name: "Pavão", group: 19, dozens: ["73", "74", "75", "76"] },
+  { name: "Peru", group: 20, dozens: ["77", "78", "79", "80"] },
+  { name: "Touro", group: 21, dozens: ["81", "82", "83", "84"] },
+  { name: "Tigre", group: 22, dozens: ["85", "86", "87", "88"] },
+  { name: "Urso", group: 23, dozens: ["89", "90", "91", "92"] },
+  { name: "Veado", group: 24, dozens: ["93", "94", "95", "96"] },
+  { name: "Vaca", group: 25, dozens: ["97", "98", "99", "00"] },
+];
 
 interface Result {
   animal: Animal;
@@ -55,16 +60,23 @@ export class AppService {
 
     this.cachedItem.subscribe((result) => {
       const r: Result = {
-        animal: lottery.find((animal) => animal.dozens.includes(result.substring(2, 4))),
-        number: result
-      } 
-      return this.client.emit('result', r)
-    })
+        animal: lottery.find((animal) =>
+          animal.dozens.includes(result.substring(2, 4))
+        ),
+        number: result,
+      };
+      // envia o resultado para o cache
+      return this.client.emit("result", r);
+    });
   }
 
-  private cachedItem = interval(5000).pipe(map(() => {
-    const result = Math.floor(Math.random() * 1000).toString().padStart(4, '0')
-    return result
-  }))
-
+  // retorna um observable que emite um valor a cada 5 segundos de um número aleatório da milhar
+  private cachedItem = interval(5000).pipe(
+    map(() => {
+      const result = Math.floor(Math.random() * 1000)
+        .toString()
+        .padStart(4, "0");
+      return result;
+    })
+  );
 }
